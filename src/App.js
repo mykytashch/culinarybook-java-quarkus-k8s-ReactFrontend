78f1b743de6759c76ef7fdd3d7ca70d9f85bb8f4
Mykyta Shcheholevatyi Oleksiiovych <tosniki91@gmail.com>
2023-10-14 14:02:47 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Welcome to Mykyta Fun
+14 октября
+
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
